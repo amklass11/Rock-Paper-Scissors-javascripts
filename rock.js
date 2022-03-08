7124657d81489624e0f8c79f43c97cca46d6ab03
man lass <lassanaa14@gmail.com>
2022-03-08 06:53:01 -0600

    fix lgoic error

--- rock.js
+++ rock.js
@@ -1,31 +1,44 @@
-var choice = ['rock', 'paper', 'scissors'];
+
+function computerPlay(choice) {
+     choice = ['rock', 'paper', 'scissors'];
+    randomItem = choice[Math.floor(Math.random() * 3)];
+    console.log(randomItem);
+
+};
 function playRound(playerSelection,computerSelection) {
 
-    if (playerSelection == 'rock' && b == 'paper') {
+    if (playerSelection === 'rock'&&computerSelection === 'paper') 
+    {
         console.log('you lose');
-    } else if (playerSelection== 'rock' && computerSelection== 'scissors') {
+    }
+     else if (playerSelection === 'rock'&&computerSelection=== 'scissors') {
         console.log('you win');
-    } else if (playerSelection== 'paper' && computerSelection== 'scissors') {
+    }
+     else if (playerSelection === 'paper'&&computerSelection=== 'scissors') {
         console.log('you lose');
-    } else if (playerSelection== 'paper' && computerSelection== 'rock') {
+    } 
+    else if (playerSelection === 'paper'&&computerSelection=== 'rock') {
         console.log('you win');
-    } else if (playerSelection== 'scissors' && computerSelection== 'paper') {
+    } 
+    else if (playerSelection === 'scissors'&&computerSelection=== 'paper') {
         console.log('you win');
-    } else if (playerSelection== 'scissors' && computerSelection== 'rock') {
+    } 
+    else if (playerSelection === 'scissors'&&computerSelection=== 'rock') {
         console.log('you lose');
-    } else (playerSelection== b) 
-    {console.log('Draw')}
-        ;
-    
-};
-function computerPlay(choice) {
-    randomItem = choice[Math.floor(Math.random() * choice.length)];
-
+    } 
+    else if(computerSelection===playerSelection)
+    {
+        console.log('Draw');
 }
+}
+    
 
-var playerSelection = "rock";
+const playerSelection = 'paper';
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
+console.log(computerSelection);
 
+console.log(playerSelection)
+console.log(playRound('rock','paper'));
 
 
